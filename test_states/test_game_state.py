32a4ba68f7908accea74792397d36566d0fc0cfd
Nick <Nickvuletich23@gmail.com>
2026-03-18 03:20:11 -0400

    added test for player death

--- test_states/test_game_state.py
+++ test_states/test_game_state.py
@@ -1,7 +1,6 @@
 import sys
 import os
 import pygame
-from states.game_state import Bullet
 
 # Add project root to Python path so tests can import from 'states'
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -10,7 +9,19 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 pygame.init()
 pygame.display.set_mode((1, 1))  # tiny window to test the asset since it needs a display
 
+# Fake app for testing
+class FakeApp:
+    def __init__(self):
+        self.width = 800
+        self.height = 600
+        self.changed_to = None
+
+    def change_state(self, state):
+        self.changed_to = state
+
 #----------Bullet Tests----------
+from states.game_state import Bullet
+
 def test_bullet_despawn():
     bullet = Bullet("assets", "basic_bullet.png", 5, (100,100), (0,-1))
 
@@ -30,3 +41,23 @@ def bullets_alive_onscreen():
 
     bullet.update()
     assert bullet.alive()
+
+#----------Player Death Test----------
+
+def test_player_dies_on_collision():
+    from states.game_state import GameState
+    from states.death_state import DeathState
+    
+    app = FakeApp()
+
+    game_state = GameState()
+    game_state.on_enter(app)
+
+    # moves the enemy into the player
+    enemy = next(iter(game_state.enemy_ships))
+    enemy.rect.center = game_state.player.rect.center
+
+    game_state.update(app, dt=0.016)
+
+    assert isinstance(app.changed_to, DeathState)
+
